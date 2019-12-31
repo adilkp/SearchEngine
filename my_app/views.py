2c594cbc5f6097cdf8aabb066102bc7c245e41aa
Adil Pathan <emb-adilpat@emb-ban-184.embitel.local>
2019-12-31 12:59:55 +0530

    Image cards implemented

--- my_app/views.py
+++ my_app/views.py
@@ -1,4 +1,6 @@
+import requests
 from django.shortcuts import render
+from bs4 import BeautifulSoup
 
 
 def home(request):
@@ -6,4 +8,10 @@ def home(request):
 
 
 def new_search(request):
-    return render(request, 'my_app/new_search.html')
+    search = request.POST.get('search')
+    print(search)
+    stuff_for_frontend = {
+        'search': search,
+    }
+    return render(request, 'my_app/new_search.html', stuff_for_frontend)
+
